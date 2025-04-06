e9fd197916ca6b28b71e6d02c8943e53b0680998
Chinmay Devrukhkar <154670619+chinmaydevrukhkar@users.noreply.github.com>
2025-04-06 17:15:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,131 +1,140 @@
-// importing modules
-const express = require("express");
-const http = require("http");
-const mongoose = require("mongoose");
-
-const app = express();
-const port = process.env.PORT || 3000;
-var server = http.createServer(app);
-const Room = require("./models/room");
-var io = require("socket.io")(server);
-
-// middle ware
-app.use(express.json());
-
-const DB =
-  "mongodb+srv://rivaan:test123@cluster0.rmhtu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
-io.on("connection", (socket) => {
-  console.log("connected!");
-  socket.on("createRoom", async ({ nickname }) => {
-    console.log(nickname);
-    try {
-      // room is created
-      let room = new Room();
-      let player = {
-        socketID: socket.id,
-        nickname,
-        playerType: "X",
-      };
-      room.players.push(player);
-      room.turn = player;
-      room = await room.save();
-      console.log(room);
-      const roomId = room._id.toString();
-
-      socket.join(roomId);
-      // io -> send data to everyone
-      // socket -> sending data to yourself
-      io.to(roomId).emit("createRoomSuccess", room);
-    } catch (e) {
-      console.log(e);
-    }
-  });
-
-  socket.on("joinRoom", async ({ nickname, roomId }) => {
-    try {
-      if (!roomId.match(/^[0-9a-fA-F]{24}$/)) {
-        socket.emit("errorOccurred", "Please enter a valid room ID.");
-        return;
-      }
-      let room = await Room.findById(roomId);
-
-      if (room.isJoin) {
-        let player = {
-          nickname,
-          socketID: socket.id,
-          playerType: "O",
-        };
-        socket.join(roomId);
-        room.players.push(player);
-        room.isJoin = false;
-        room = await room.save();
-        io.to(roomId).emit("joinRoomSuccess", room);
-        io.to(roomId).emit("updatePlayers", room.players);
-        io.to(roomId).emit("updateRoom", room);
-      } else {
-        socket.emit(
-          "errorOccurred",
-          "The game is in progress, try again later."
-        );
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  });
-
-  socket.on("tap", async ({ index, roomId }) => {
-    try {
-      let room = await Room.findById(roomId);
-
-      let choice = room.turn.playerType; // x or o
-      if (room.turnIndex == 0) {
-        room.turn = room.players[1];
-        room.turnIndex = 1;
-      } else {
-        room.turn = room.players[0];
-        room.turnIndex = 0;
-      }
-      room = await room.save();
-      io.to(roomId).emit("tapped", {
-        index,
-        choice,
-        room,
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  });
-
-  socket.on("winner", async ({ winnerSocketId, roomId }) => {
-    try {
-      let room = await Room.findById(roomId);
-      let player = room.players.find(
-        (playerr) => playerr.socketID == winnerSocketId
-      );
-      player.points += 1;
-      room = await room.save();
-
-      if (player.points >= room.maxRounds) {
-        io.to(roomId).emit("endGame", player);
-      } else {
-        io.to(roomId).emit("pointIncrease", player);
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  });
-});
-
-mongoose
-  .connect(DB)
-  .then(() => {
-    console.log("Connection successful!");
-  })
-  .catch((e) => {
-    console.log(e);
-  });
-
-server.listen(port, "0.0.0.0", () => {
-  console.log(`Server started and running on port ${port}`);
-});
+require("dotenv").config();
+ const exp = require("constants");
+ const mongoose = require("mongoose");
+ const express = require("express");
+ const http = require("http");
+ 
+ const app = express();
+ const port = process.env.PORT || 3000;
+ var server = http.createServer(app);
+ const Room = require("./models/room");
+ var io = require("socket.io")(server);
+ 
+ //middleware
+ app.use(express.json());
+ 
+ io.on('connection', (socket)=>{
+     console.log("connected");
+     socket.on('createRoom', async({nickname})=>{
+         console.log(nickname);
+         //room is created
+         try{
+         let room = new Room();
+         let player = {
+             socketID: socket.id,
+             nickname,
+             playerType: 'X',
+         };
+ 
+         room.players.push(player);
+         room.turn = player;
+ 
+         room = await room.save(); //save to db
+         console.log(room);
+         const roomId = room._id.toString();
+ 
+         socket.join(roomId);
+         // Tell our client that room has been created, go to the next page
+         io.to(roomId).emit("createRoomSuccess", room);
+         }catch(e){
+             console.log(e);
+         }
+     });
+ 
+     socket.on('joinRoom', async({nickname, roomId})=>{
+         try{
+             if(!roomId.match(/^[0-9a-fA-F]{24}$/)){
+                 socket.emit('errorOccured', 'Please enter a valid Room ID.');
+                 return;
+             }
+             let room = await Room.findById(roomId);
+              
+             if(room.isJoin){
+                 let player ={
+                     nickname,
+                     socketID: socket.id,
+                     playerType: 'O',
+                 }
+                 socket.join(roomId);
+                 room.players.push(player);
+                 room.isJoin = false;
+                 room = await room.save();
+                 // Tell our client that room has been created, go to the next page
+                 io.to(roomId).emit("joinRoomSuccess", room);
+                 io.to(roomId).emit("updatePlayers", room.players);
+                 io.to(roomId).emit("updateRoom", room);
+ 
+ 
+             }else{
+                 socket.emit('errorOccured', 'The game is in Progress, try again later')
+             }
+         }catch(e){
+             console.log(e);
+         }
+     });
+ 
+     socket.on('tap', async({index, roomId})=>{
+         try{
+             let room = await Room.findById(roomId);
+             let choice = room.turn.playerType; // X or O
+             if(room.turnIndex == 0){
+                 room.turn = room.players[1];
+                 room.turnIndex = 1;
+             }
+             else{
+                 room.turn = room.players[0];
+                 room.turnIndex = 0;
+     
+             }
+             room = await room.save();
+             
+             io.to(roomId).emit('tapped', {
+                 index,
+                 choice,
+                 room,
+             });
+     
+         }catch(e){
+             console.log(e);
+         }
+     
+     });
+ 
+ 
+     socket.on('winner', async({winnerSocketId, roomId})=>{
+         try{
+             let room = await Room.findById(roomId);
+             let player = room.players.find((player)=> player.socketID == winnerSocketId);
+             player.points+=1;
+             room = await room.save();
+             if(player.points >= room.maxRound){
+                 io.to(roomId).emit('endGame', player);
+             }
+             else{
+                 io.to(roomId).emit('pointIncrease', player);
+             }
+         }catch(e){
+             console.log(e);
+         }
+     });
+     
+ });
+ 
+ 
+ 
+ 
+ mongoose.connect(process.env.MONGODB_URL).then(()=>{
+     console.log("Connection successful");
+ }).catch((e)=> {
+     console.log(e);
+ })
+ 
+ server.listen(port, '0.0.0.0', () => {
+     console.log(`Server started and running on port ${port}`);
+ });
+ 
+ 
+ 
+ 
+ 
+ 
