e055368716c149b93a4478b58a563ca5c12f8fe5
Chinmay Devrukhkar <154670619+chinmaydevrukhkar@users.noreply.github.com>
2025-04-06 17:12:34 +0530

    Update room.js

--- models/room.js
+++ models/room.js
@@ -25,36 +25,7 @@ const roomSchema = new mongoose.Schema({
     type: Number,
     default: 0,
   },
-  roomCode: {
-    type: String,
-    unique: true,
-  },
 });
-
-// Pre-save hook to generate a short, unique roomCode
-roomSchema.pre('save', async function (next) {
-    if (!this.roomCode) {
-      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-      let code;
-      let isUnique = false;
-  
-      while (!isUnique) {
-        code = Array.from({ length: 6 }, () =>
-          characters.charAt(Math.floor(Math.random() * characters.length))
-        ).join('');
-  
-        const existing = await mongoose.models.Room.findOne({ roomCode: code });
-        if (!existing) {
-          isUnique = true;
-        }
-      }
-  
-      this.roomCode = code;
-    }
-  
-    next();
-  });
-  
 
 const roomModel = mongoose.model("Room", roomSchema);
 module.exports = roomModel;
